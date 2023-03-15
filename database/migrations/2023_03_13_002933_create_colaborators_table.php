c34cdd6b90f6151dec6614f7e83ec741caeeb708
Yurisn1 <dev.yurisn@gmail.com>
2023-03-15 12:43:55 -0300

    ✨ Added migrations

---
+++ database/migrations/2023_03_13_002933_create_colaborators_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateColaboratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colaborators', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('cpf')->unique();
+            $table->string('phone');
+            $table->string('stacks');
+            $table->boolean('verification')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colaborators');
+    }
+}
